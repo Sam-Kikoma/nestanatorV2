ae52a5f0570dc3e41f95a305edc609df23366344
Sam Kikoma <kikomasamson@gmail.com>
2026-06-01 23:20:29 +0200

    feat: integrate LoggerService into AuthGuard for improved logging

--- random-emoji-api/src/common/auth/auth.guard.ts
+++ random-emoji-api/src/common/auth/auth.guard.ts
@@ -1,20 +1,22 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { LoggerService } from '../../logger.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly logger: LoggerService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log(`Guard: Checking authentication...`);
+    this.logger.info('Guard: Checking authentication...');
     const request = context.switchToHttp().getRequest<Request>();
     const apiKey = request.headers['x-api-key'];
     if (apiKey !== 'SECRET_SAUCE') {
-      console.log(`Guard: Failed authentication`);
+      this.logger.info('Guard: Failed authentication');
       return false;
     }
-    console.log(`Guard: Passed authentication`);
+    this.logger.info('Guard: Passed authentication');
     return true;
   }
 }
