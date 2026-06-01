03b861d26935d8248c4bcda730f289c2c24d0527
Sam Kikoma <kikomasamson@gmail.com>
2026-06-01 13:41:26 +0200

    feat: add EmojiValidationPipe for validating emoji input values

---
+++ random-emoji-api/src/common/emoji-validation/emoji-validation.pipe.spec.ts
@@ -0,0 +1,35 @@
+import { BadRequestException } from '@nestjs/common';
+import { EmojiValidationPipe } from './emoji-validation.pipe';
+
+describe('EmojiValidationPipe', () => {
+  const emojiPipe = new EmojiValidationPipe();
+
+  it('should be defined', () => {
+    expect(emojiPipe).toBeDefined();
+  });
+
+  it('should return undefined if no value is passed', () => {
+    const result = emojiPipe.transform(undefined);
+    expect(result).toBeUndefined();
+  });
+
+  it('should throw a BadRequestError if the value is not a number', () => {
+    const result = () => emojiPipe.transform('banana');
+    expect(result).toThrow(BadRequestException);
+  });
+
+  it('should throw a BadRequestError if the value is less than 0', () => {
+    const result = () => emojiPipe.transform(-1);
+    expect(result).toThrow(BadRequestException);
+  });
+
+  it('should throw a BadRequestError if the value is greater than 11', () => {
+    const result = () => emojiPipe.transform(12);
+    expect(result).toThrow(BadRequestException);
+  });
+
+  it('should return the respective string input as a number', () => {
+    const result = emojiPipe.transform('5');
+    expect(result).toEqual(5);
+  });
+});
---
+++ random-emoji-api/src/common/emoji-validation/emoji-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class EmojiValidationPipe implements PipeTransform {
+  transform(value: any) {
+    if (!value) {
+      return;
+    }
+    if (isNaN(value)) {
+      throw new BadRequestException(
+        `Validation failed: ${value} is not a number`,
+      );
+    }
+    if (value < 0 || value > 11) {
+      throw new BadRequestException(
+        `Validation failed: ${value} is not within range`,
+      );
+    }
+    console.log('Pipe validation passed');
+    return Number(value);
+  }
+}
