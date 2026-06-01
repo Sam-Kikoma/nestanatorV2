be7cf89c1ffa8ef1cb85961cebfc967f92569a14
Sam Kikoma <kikomasamson@gmail.com>
2026-06-01 13:41:48 +0200

    feat: update getEmoji method to include browser header and index validation

--- random-emoji-api/src/app.controller.ts
+++ random-emoji-api/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EmojiValidationPipe } from './common/emoji-validation/emoji-validation.pipe';
+import type { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getEmoji();
+  getEmoji(
+    @Req() request: Request,
+    @Query('index', EmojiValidationPipe) index?: number,
+  ) {
+    return {
+      emoji: this.appService.getEmoji(),
+      browser: request.headers.browser,
+    };
   }
 }
